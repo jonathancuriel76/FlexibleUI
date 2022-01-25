4ad780a8206ccaadf2124177f49aff6311062685
Jonathan Curiel <jonathancuriel76@gmail.com>
2022-01-25 08:14:26 -0800

    Adding ButtonData asset updating Sample Scene
    Adding ButtonData asset updating Sample Scene

--- FlexibleUI/Assets/Editor/FlexibleUIEditor/FlexibleUITools.cs
+++ FlexibleUI/Assets/Editor/FlexibleUIEditor/FlexibleUITools.cs
@@ -1,30 +1,29 @@
 using Assets.Scripts.FlexibleUI;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Editor.FlexibleUIEditor
 {
     public static class FlexibleUITools
     {
-        [MenuItem("GameObject/UI/FlexibleUI Image", false, 10)]
+        [MenuItem("GameObject/UI/FlexibleUI/FlexibleUI Image", false, 10)]
         private static void CreateFlexibleUIImage(MenuCommand menuCommand)
         {
             var go = new GameObject("Image");
             GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
             Undo.RegisterCreatedObjectUndo(go, "Create" + go.name);
             Selection.activeObject = go;
-            //go.AddComponent<Image>();
             go.AddComponent<FlexibleUIImage>();
         }
 
-        [MenuItem("GameObject/UI/FlexibleUI TextMeshPro", false, 10)]
+        [MenuItem("GameObject/UI/FlexibleUI/FlexibleUI TextMeshPro", false, 10)]
         private static void CreateFlexibleUITextMeshPro(MenuCommand menuCommand)
         {
             var go = new GameObject("Text");
             GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
             Undo.RegisterCreatedObjectUndo(go, "Create" + go.name);
             Selection.activeObject = go;
-            //go.AddComponent<TextMeshProUGUI>();
             go.AddComponent<FlexibleUITextMeshPro>();
         }
     }
